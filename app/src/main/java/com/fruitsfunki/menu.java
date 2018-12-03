f464b9254ea7aa7570be820bbb7c8b7675c3df42
Rhythm45 <darkar05@live.cl>
2018-12-03 18:20:46 -0300

    menu + botones

--- app/src/main/java/com/fruitsfunki/menu.java
+++ app/src/main/java/com/fruitsfunki/menu.java
@@ -1,7 +1,9 @@
 package com.fruitsfunki;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class menu extends AppCompatActivity {
 
@@ -10,4 +12,18 @@ public class menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
+
+
+
+    public void menu(View v) {
+
+
+
+        Intent batido1 = new Intent(getApplicationContext(), batido1.class);
+        startActivity(batido1);
+
+
+
+    }
+
 }
