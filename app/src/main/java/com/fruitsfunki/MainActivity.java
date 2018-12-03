c8ae33735d3cf34308ba95c415820ac4c4b4d7dc
Rhythm45 <darkar05@live.cl>
2018-12-03 18:13:19 -0300

    pantalla principal llama a menu

--- app/src/main/java/com/fruitsfunki/MainActivity.java
+++ app/src/main/java/com/fruitsfunki/MainActivity.java
@@ -1,9 +1,13 @@
 package com.fruitsfunki;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -18,7 +22,10 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton btn_ingresar;
+
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Object View;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +33,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("Nombre_batido", "chirimoya express ");
-        user.put("Descripcion", "Si estas despuesto a beneficiarte de sus propiedades, una buena manera de comenzar el dia es con este batido");
-        user.put("ingredientes", "chirimoya, salsa de frutilla, leche");
+        FloatingActionButton btn_ingresar = findViewById(R.id.btn_ingresar);
+        btn_ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-// Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("mensaje", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("mensaje", "Error adding document", e);
-                    }
-                });
+                Intent menu = new Intent(getApplicationContext(), menu.class);
+                startActivity(menu);
+
+            }
+        });
+
+
+
+
+
+
+
+}
+
+
+    public void menu(View v) {
+
+
+
+        Intent menu = new Intent(getApplicationContext(), menu.class);
+        startActivity(menu);
+
+
+
     }
+
+
+
 }
