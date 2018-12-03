20a508c2a8e33a27fa2f027ba6c9af673426843a
SacroMetal <jeresn@gmail.com>
2018-12-03 17:49:04 -0300

    Agregando nuevos cambios

--- app/src/main/java/com/fruitsfunki/MainActivity.java
+++ app/src/main/java/com/fruitsfunki/MainActivity.java
@@ -3,6 +3,9 @@ package com.fruitsfunki;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+    
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
