802a03eae6e6e6e3fda89cedfa6ddf9bed4b1a02
SacroMetal <jeresn@gmail.com>
2018-12-03 18:15:53 -0300

    descargando archivos

--- app/src/main/java/com/fruitsfunki/MainActivity.java
+++ app/src/main/java/com/fruitsfunki/MainActivity.java
@@ -1,54 +1,16 @@
 package com.fruitsfunki;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
-
+    
 
 
 public class MainActivity extends AppCompatActivity {
-
-    FloatingActionButton btn_ingresar;
-
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("Nombre_batido", "chirimoya express ");
-        user.put("Descripcion", "Si estas despuesto a beneficiarte de sus propiedades, una buena manera de comenzar el dia es con este batido");
-        user.put("ingredientes", "chirimoya, salsa de frutilla, leche");
-
-// Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("mensaje", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("mensaje", "Error adding document", e);
-                    }
-                });
     }
 }
