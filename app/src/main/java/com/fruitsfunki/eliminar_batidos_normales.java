d3f97670a5d503aa8a39bf7b9036b7b8fb7c145a
Rhythm45 <darkar05@live.cl>
2018-12-04 17:29:24 -0300

    ultimo

---
+++ app/src/main/java/com/fruitsfunki/eliminar_batidos_normales.java
@@ -0,0 +1,13 @@
+package com.fruitsfunki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class eliminar_batidos_normales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar_batidos_normales);
+    }
+}
